6d739ba2ed34d5f586dc8f600a6a146a61de00c6
John Pepp <jrpepp@pepster.com>
2017-05-31 15:40:18 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -15,14 +15,25 @@ $myCalendar->phpDate();
 
 $submit = filter_input(INPUT_POST, 'submit', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 if (isset($submit) && $submit === 'submit') {
-    $data['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $data['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $data['phone'] = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $data['website'] = filter_input(INPUT_POST, 'website', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $data['reason'] = filter_input(INPUT_POST, 'reason', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $data['comments'] = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    /* The Following to get response back from Google recaptcah */
+    $url = "https://www.google.com/recaptcha/api/siteverify";
 
-    $send = new Email($data, $transport);
+    $remoteServer = filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_SANITIZE_URL);
+    $response = file_get_contents($url . "?secret=" . PRIVATE_KEY . "&response=" . \htmlspecialchars($_POST['g-recaptcha-response']) . "&remoteip=" . $remoteServer);
+    $recaptcha_data = json_decode($response);
+    /* The actual check of the recaptcha */
+    if (isset($recaptcha_data->success) && $recaptcha_data->success === TRUE) {
+        $data['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $data['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $data['phone'] = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $data['website'] = filter_input(INPUT_POST, 'website', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $data['reason'] = filter_input(INPUT_POST, 'reason', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $data['comments'] = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        $send = new Email($data, $transport);
+    } else {
+        $success = "You're not a human!";
+    }
 }
 ?>
 
@@ -33,6 +44,7 @@ if (isset($submit) && $submit === 'submit') {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>A Slice of Technology</title>
         <link rel="stylesheet" href="css/stylesheet.css">
+        <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
         <div id="header">
@@ -89,7 +101,8 @@ if (isset($submit) && $submit === 'submit') {
                         </div>
 
                         <label class="textBox" for="comments">Comments</label>
-                        <textarea name="comments" id="comments" spellcheck="true" tabindex="6" required="required"></textarea>             
+                        <textarea name="comments" id="comments" spellcheck="true" tabindex="6" required="required"></textarea> 
+                        <div class="g-recaptcha" data-sitekey="6LfPlQoUAAAAAPgD3PpnQ_uGTzc87UALiFgQ3XnK"></div>
                         <input type="submit" name="submit" value="submit" tabindex="7">
                     </fieldset>
 
